0c089d09998d8e7cdb9a2b2c4e167ad6f6eb8637
Mary Dickson Diaz <mdickson@gmail.com>
2015-11-12 09:49:54 -0800

    cleanup

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'zurb-foundation'
 gem 'foundation-rails'
+gem 'minitest-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -37,6 +38,7 @@ gem 'foundation-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem "minitest-rails-capybara"
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905233715) do
+ActiveRecord::Schema.define(version: 20151014161126) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
